5b5cfe5315fa90d34522526eb1f4f27ea0a7ee14
˗ˏˋShubha ´ˎ˗ <93957744+ShubhaPokharel@users.noreply.github.com>
2023-05-19 20:03:06 -0400

    Update StudentReport.java

--- StudentReport.java
+++ StudentReport.java
@@ -17,13 +17,13 @@ class StudentReport{
 	}
 	public static void main(String[] args){
 		StudentReport student = new StudentReport();
-		// student.studentGrade("Sushil",1710,50.8,83.9,1.9);
-		// student.studentGrade("Anu",1712,100.8,69.65,64.9);
-		// student.studentGrade("Shubha",1713,100.53,100.35,100.35);
-		// student.studentGrade("Aruna",1714,20.4,50.4,63.9);
-		// student.studentGrade("Maki",1715,78.8,93.6,1.53);
-		// student.studentGrade("Fred",1716,70.4,24.6,46.8);
-		student.studentGrade("Bil",1717,24.53,47.7,1.9);
+		 student.studentGrade("Sushil",1710,50.8,83.9,1.9);
+		 student.studentGrade("Anu",1712,100.8,69.65,64.9);
+		 student.studentGrade("Shubha",1713,100.53,100.35,100.35);
+		 student.studentGrade("Aruna",1714,20.4,50.4,63.9);
+		 student.studentGrade("Maki",1715,78.8,93.6,1.53);
+		 student.studentGrade("Fred",1716,70.4,24.6,46.8);
+		 student.studentGrade("Bil",1717,24.53,47.7,1.9);
 
 
 	}
